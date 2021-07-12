c4d83e9ad7e7ee9bc5b8d5eb9a04b8290ee43d98
Marek <marekbf3@gmail.com>
2021-07-12 12:23:16 +0200

    Modify secrets source
    * secrets from file / env vars

--- src/firebase.ts
+++ src/firebase.ts
@@ -1,14 +1,46 @@
 import firebase from 'firebase';
-import firebaseConfig from './config/secrets/firebase.json';
-import firebaseStorage from './config/secrets/firebaseStorage.json';
-import serviceAccount from './config/secrets/serviceAccountKey.json';
 import admin from 'firebase-admin';
+import * as fs from 'fs';
+
+function firebaseStorage() {
+  if(fs.existsSync('./config/secrets/firebaseStorage.json')){
+    return JSON.parse(fs.readFileSync('./config/secrets/firebaseStorage.json', "utf-8"))
+  }
+  const envVar = process.env["FIREBASE_STORAGE"]
+  if(!envVar){
+    throw new Error("Cannot find FIREBASE_STORAGE variable")
+  }
+  return JSON.parse(envVar.toString())
+}
+
+function firebaseConfig() {
+  if(fs.existsSync('./config/secrets/firebase.json')){
+    return JSON.parse(fs.readFileSync('./config/secrets/firebase.json', "utf-8"))
+  }
+  const envVar = process.env["FIREBASE_CONFIG"]
+  if(!envVar){
+    throw new Error("Cannot find FIREBASE_CONFIG variable")
+  }
+  return JSON.parse(envVar.toString())
+}
+
+function serviceAccount(): any {
+  if(fs.existsSync('./config/secrets/serviceAccountKey.json')){
+    return JSON.parse(fs.readFileSync('./config/secrets/serviceAccountKey.json', "utf-8"))
+  }
+  const envVar = process.env["SERVICE_ACCOUNT"]
+  if(!envVar){
+    throw new Error("Cannot find SERVICE_ACCOUNT variable")
+  }
+  return JSON.parse(envVar.toString())
+}
+
 
 admin.initializeApp({
-// @ts-ignore
-  credential: admin.credential.cert(serviceAccount),
-  storageBucket: firebaseStorage.storageBucket,
+  // @ts-ignore
+  credential: admin.credential.cert(serviceAccount()),
+  storageBucket: firebaseStorage().storageBucket,
 });
 
-firebase.initializeApp(firebaseConfig);
-export {firebase, admin}; 
+firebase.initializeApp(firebaseConfig());
+export { firebase, admin };
