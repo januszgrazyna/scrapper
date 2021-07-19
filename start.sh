b852a32ee967f69a73cba21fd9747c5e6dbbeb87
Marek <marekbf3@gmail.com>
2021-07-19 20:34:02 +0200

    Add runfile and run script

--- start.sh
+++ start.sh
@@ -4,6 +4,8 @@
 CMD=$(cat ./run.txt | sed -e "s/.*PM:\s*\(.*\)/\1/" | sed -e "s/.*AM:\s*\(.*\)/\1/")
 echo "Command: $CMD"
 
-pushd dist
+pushd dist 2>&1 > /dev/null
 node src/index.js $CMD
-popd
+popd 2>&1 > /dev/null
+
+exit 0
