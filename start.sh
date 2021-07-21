1e8c735c23eccd14df5c5a24eb90443ae8a82ffe
Marek <marekbf3@gmail.com>
2021-07-21 12:52:24 +0200

    Add ignore run option to run script

--- start.sh
+++ start.sh
@@ -1,11 +1,20 @@
 #!/bin/bash
 
 
-CMD=$(cat ./run.txt | sed -e "s/.*PM:\s*\(.*\)/\1/" | sed -e "s/.*AM:\s*\(.*\)/\1/")
+FILE_CONTENTS=$(cat ./run.txt)
+
+if [[ "${FILE_CONTENTS:0:1}" == "?" ]]; then
+    echo "Runfile starts with '?'. Run will not proceed."
+    exit 0
+fi
+
+CMD=$(echo "$FILE_CONTENTS" | sed -e "s/.*PM:\s*\(.*\)/\1/" | sed -e "s/.*AM:\s*\(.*\)/\1/")
 echo "Command: $CMD"
 
 pushd dist 2>&1 > /dev/null
+set -e
 node src/index.js $CMD
+set +e
 popd 2>&1 > /dev/null
 
 exit 0
