31bcc18bf594481b733a15d79547fdfe705a4eb5
Marek <marekbf3@gmail.com>
2021-09-21 12:11:21 +0200

    test

--- download_scrappers.sh
+++ download_scrappers.sh
@@ -5,3 +5,4 @@ npm run build-impldownload
 APPROOT=$(pwd)
 IFS=$'\n' read -ra arr -d '' <scrapper_download_list.txt
 npm run start-impldownload "$APPROOT" "${arr[@]}"
+cat impls/Allegro/AllegroItemsPage.ts
