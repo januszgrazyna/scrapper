aed22f58956a64d54ac0138bc5964ecc0e145531
Marek <marekbf3@gmail.com>
2021-09-25 14:26:58 +0200

    Remove verbose parts from download script

--- download_scrappers.sh
+++ download_scrappers.sh
@@ -5,4 +5,3 @@ npm run build-impldownload
 APPROOT=$(pwd)
 IFS=$'\n' read -ra arr -d '' <scrapper_download_list.txt
 npm run start-impldownload "$APPROOT" "${arr[@]}"
-cat impls/Allegro/AllegroItemsPage.ts
