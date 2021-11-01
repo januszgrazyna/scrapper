27469f545f24a9d3e27b133a6bb4fd385c3a4337
Marek Pękala <marekbf3@gmail.com>
2021-11-01 08:58:09 +0100

    Add diag script and action

---
+++ diag_start.sh
@@ -0,0 +1,14 @@
+#!/bin/sh
+set -e
+
+ngrok tcp 8080 &
+echo "waiting for ngrok start"
+sleep 15
+url=$(curl -s http://127.0.0.1:4040/api/tunnels | jq -r '.tunnels[] | .public_url')
+
+echo "starting mitmdump"
+mitmdump > /dev/null &
+
+echo "tunnel at $url"
+
+sleep inf
---
+++ ngrok_auth.sh
@@ -0,0 +1,3 @@
+#!/bin/bash
+
+ngrok authtoken $NGROK_AUTHTOKEN
