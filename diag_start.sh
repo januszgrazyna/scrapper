08eb352ccd0e026be2c6e5a968b773696d399c4c
Marek Pękala <marekbf3@gmail.com>
2021-11-01 09:49:03 +0100

    Add ssl insecure opt

--- diag_start.sh
+++ diag_start.sh
@@ -1,13 +1,13 @@
 #!/bin/sh
 set -e
 
-ngrok tcp 8080 &
+ngrok tcp 8080 > /dev/null &
 echo "waiting for ngrok start"
 sleep 15
 url=$(curl -s http://127.0.0.1:4040/api/tunnels | jq -r '.tunnels[] | .public_url')
 
 echo "starting mitmdump"
-mitmdump > /dev/null &
+mitmdump --set ssl_insecure=true > /dev/null &
 
 echo "tunnel at $url"
 
